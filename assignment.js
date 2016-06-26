0572489bdbaf4c50b7e1515ddca583392d32e5bd
Arjun Krishna Babu <arjunkrishnababu96@gmail.com>
2016-06-26 18:20:50 +0530

    Displaying a dynamic categories page

--- assignment.js
+++ assignment.js
@@ -1,1 +1,24 @@
 // put your javascript code here
+/*
+ Contains javascript code to handle all the image-switching logic
+ */
+
+var cat_list_template;
+
+function showTemplate(template, data) {
+    var html = template(data);
+    $("#content").html(html);
+}
+
+$(document).ready(function () {
+    console.log("assignment.js is ready!")
+
+    var source = $("#cat-list-template").html();
+    cat_list_template = Handlebars.compile(source);
+
+
+    $("#categories-tab").click(function () {
+        console.log("Categories tab clicked!");
+        showTemplate(cat_list_template, animals_data);
+    })
+});
